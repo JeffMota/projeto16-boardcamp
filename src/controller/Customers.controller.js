312743b66bb70110c41f5e9cd7f062478d2d2ad4
JeffMota <jmota906@gmail.com>
2023-02-11 13:12:42 -0300

    fix: Atualizar cliente

--- src/controller/Customers.controller.js
+++ src/controller/Customers.controller.js
@@ -54,8 +54,12 @@ export async function updateCustomer(req, res) {
     const { name, phone, cpf, birthday } = req.body
 
     try {
-        const alreadyExist = await db.query(`SELECT * FROM customers WHERE cpf = $1 AND name <> $2;`, [cpf, name])
-        if (alreadyExist.rows.length > 0) return res.sendStatus(409)
+        const alreadyExist = await db.query(`SELECT * FROM customers WHERE cpf = $1;`, [cpf])
+        if (alreadyExist.rows.length > 0) {
+            if (name !== alreadyExist.rows[0].name && phone !== alreadyExist.rows[0].phone && birthday !== alreadyExist.rows[0].birthday) {
+                return res.sendStatus(409)
+            }
+        }
 
         await db.query(`UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5;`,
             [name, phone, cpf, birthday, id])
