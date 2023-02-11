1fa551a7048dba51db878d1f7f69408ff86523af
JeffMota <jmota906@gmail.com>
2023-02-11 09:55:55 -0300

    refactor: Otimização query do pg

--- src/controller/Rentals.controller.js
+++ src/controller/Rentals.controller.js
@@ -4,33 +4,66 @@ import { db } from "../config/database.js"
 export async function listRentals(req, res) {
     try {
 
-        const listRentals = await db.query(`SELECT * FROM rentals;`)
-        const listCustomers = await db.query(`SELECT * FROM customers;`)
-        const listGames = await db.query(`SELECT * FROM games;`)
+        const unformatList = await db.query(
+            `
+            SELECT rentals.*, 
+            customers.name AS customer_name, 
+            customers.id AS customer_id, 
+            games.name AS game_name, 
+            games.id AS game_id 
+            FROM rentals JOIN customers ON rentals."customerId" = customers.id 
+            JOIN games ON rentals."gameId" = games.id;
+            `
+        )
 
-        const result = []
-
-        listRentals.rows.map(rent => {
-
-            const custumer = listCustomers.rows.find(cust => cust.id === rent.customerId)
-            const game = listGames.rows.find(game => game.id === rent.gameId)
-
-            let aux = {
-                ...rent,
+        const formatedList = unformatList.rows.map(rent => {
+            return {
+                id: rent.id,
+                customerId: rent.customerId,
+                gameId: rent.gameId,
+                rentDate: rent.rentDate,
+                daysRented: rent.daysRented,
+                returnDate: rent.returnDate,
+                originalPrice: rent.originalPrice,
+                delayFee: rent.delayFee,
                 customer: {
-                    id: custumer.id,
-                    name: custumer.name
+                    id: rent.customer_id,
+                    name: rent.customer_name
                 },
                 game: {
-                    id: game.id,
-                    name: game.name
+                    id: rent.game_id,
+                    name: rent.game_name
                 }
             }
-
-            result.push(aux)
         })
 
-        res.send(result)
+        // const listRentals = await db.query(`SELECT * FROM rentals;`)
+        // const listCustomers = await db.query(`SELECT * FROM customers;`)
+        // const listGames = await db.query(`SELECT * FROM games;`)
+
+        // const result = []
+
+        // listRentals.rows.map(rent => {
+
+        //     const custumer = listCustomers.rows.find(cust => cust.id === rent.customerId)
+        //     const game = listGames.rows.find(game => game.id === rent.gameId)
+
+        //     let aux = {
+        //         ...rent,
+        //         customer: {
+        //             id: custumer.id,
+        //             name: custumer.name
+        //         },
+        //         game: {
+        //             id: game.id,
+        //             name: game.name
+        //         }
+        //     }
+
+        //     result.push(aux)
+        // })
+
+        res.send(formatedList)
 
     } catch (error) {
         res.status(500).send(error.message)
